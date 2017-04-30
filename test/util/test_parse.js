e1d4a8eb2093a3f3e23c10a1407978aa4130ff6f
chrisdevwords <chrisedwards@Chriss-MacBook-Pro.local>
2017-04-30 11:11:56 -0400

    #5 full test coverage for utils

--- test/util/test_parse.js
+++ test/util/test_parse.js
@@ -1,37 +1,41 @@
 const { describe, it } = require('mocha');
 const { expect, config } = require('chai');
-const request = require('request-promise-native');
 
 const{ parseFormString } = require('../../src/util/parse');
 
 const context = describe;
 
+
 config.includeStack = true;
 
-describe('The Index Lambda Handler', () => {
+describe('The Parse Utils', () => {
 
-    const str = 'token=D8B7E8A429E&command=%2Ftest&text=https%3A%2F%2Fopen.spotify.com%2Ftrack%2F4o5jtBnLWT8OFkWNM2otW1'
-    const testText = 'https://open.spotify.com/track/4o5jtBnLWT8OFkWNM2otW1';
-    const testToken = 'D8B7E8A429E';
-    const testCommand = '/test'
+    describe('#parseFormString', () => {
 
-    context('with a form string', () => {
-        it('extracts the token', (done) => {
-            const { token } = parseFormString(str);
-            expect(token).to.eq(testToken);
-            done();
+        const str = 'token=D8B7E8A429E&command=%2Ftest&' +
+            'text=https%3A%2F%2Fopen.spotify.com%2Ftrack%2F4o5jtBnLWT8OFkWNM2otW1';
+        const testText = 'https://open.spotify.com/track/4o5jtBnLWT8OFkWNM2otW1';
+        const testToken = 'D8B7E8A429E';
+        const testCommand = '/test'
+
+        context('with a form string', () => {
+            it('extracts the token', (done) => {
+                const { token } = parseFormString(str);
+                expect(token).to.eq(testToken);
+                done();
+            });
+
+            it('extracts a command value', (done) => {
+                const { command } = parseFormString(str);
+                expect(command).to.eq(testCommand);
+                done();
+            });
+
+            it('can extract a url', (done) => {
+                const { text } = parseFormString(str);
+                expect(text).to.eq(testText);
+                done();
+            })
         });
-
-        it('extracts a command value', (done) => {
-            const { command } = parseFormString(str);
-            expect(command).to.eq(testCommand);
-            done();
-        });
-
-        it('can extract a url', (done) => {
-            const { text } = parseFormString(str);
-            expect(text).to.eq(testText);
-            done();
-        })
     });
 });
