a79dfb62c999f61fa6ae13ad75e03df3dc800dd6
chrisdevwords <chrisedwards@Chriss-MacBook-Pro.local>
2017-04-30 11:11:25 -0400

    #5 making sure application/json header is always present by default

--- src/util/lambda.js
+++ src/util/lambda.js
@@ -3,10 +3,10 @@ const DEFAULT_HEADERS = {
     'Content-Type': 'application/json'
 };
 
-function response(body, statusCode = 200, headers = DEFAULT_HEADERS) {
+function response(body, statusCode = 200, headers = {}) {
     return {
         statusCode,
-        headers,
+        headers: Object.assign({}, DEFAULT_HEADERS, headers),
         body: JSON.stringify(
             Object.assign({ statusCode }, body)
         )
