a424e5037f0b2de454170e7e62d0c6298fe8dc62
Edwards, Chris <Christopher.Edwards@nike.com>
2017-09-02 16:55:32 -0400

    #19 adding a createRadio station spec

---
+++ test/radio/test_createRadioStation.js
@@ -0,0 +1,5 @@
+const { beforeEach, afterEach, describe, it } = require('mocha');
+const { expect, config } = require('chai');
+const request = require('request-promise-native');
+const sinon = require('sinon');
+
